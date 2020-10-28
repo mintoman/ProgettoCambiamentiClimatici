c9809f5c8e1f69d522dd81066c8b07cfff961a28
Gianluca <mitomane@gmail.com>
2020-10-28 22:30:34 +0100

    + Player Grid Movement script
    + sprites: seed, garbage
    + tiles: base terreno
    + prefabs

---
+++ PlantATree/Assets/Scripts/GridMovement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    
+    private bool isMoving;
+    private Vector3 originPos;
+    private Vector3 targetPos;
+    public float timeToMove = 0.25f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+    }
+   
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        originPos = transform.position;
+        targetPos = originPos + direction;
+
+        while(elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(originPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
