cb968a8f57e5aab89f20f01857ca5b244b29f770
Gianluca <mitomane@gmail.com>
2020-10-29 08:13:02 +0100

    + TileData ScriptableObjects , MouseManager
    + TileData ScriptableObjects: Aggiunti oggetti per salvare informazioni specifiche per tipo di tile
    + MouseManager:  cliccando con il mouse controlla le informazioni salvate

---
+++ PlantATree/Assets/Scripts/MouseClickManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MouseClickManager : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase blackTile;
+
+    [SerializeField]
+    private List<TileData> tileDatas;
+
+    private Dictionary<TileBase, TileData> dataFromTiles;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+
+        foreach(var tileData in tileDatas)
+        {
+            foreach (var tile in tileData.tiles)
+            {
+                dataFromTiles.Add(tile,tileData);//the tile will be the key the tileData the value
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = tilemap.WorldToCell(mousePosition);
+
+            TileBase clickedTile = tilemap.GetTile(gridPosition);
+
+            float walkingSpeed = dataFromTiles[clickedTile].walkingSpeed;
+
+            print("At position: "+ gridPosition+ " clickedTile " + clickedTile);
+            print("Walking Speed: " + walkingSpeed + " on clickedTile " + clickedTile);
+
+            tilemap.SetTile(gridPosition, blackTile);
+        }
+    }
+}
